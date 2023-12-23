4af2053a85efd1b3ca8c8a48111e213fec71fce7
Shiv Arora <coe211231.cse.coe@cgc.edu.in>
2023-12-23 22:33:56 +0530

    Main.java code added and some layout changes

--- app/src/main/java/com/example/greeting_app/MainActivity.java
+++ app/src/main/java/com/example/greeting_app/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.greeting_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText edittext;
+    private  Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        button = findViewById(R.id.button);
+        edittext = findViewById(R.id.Edittext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputName = edittext.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome " + inputName +" to the Application", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
